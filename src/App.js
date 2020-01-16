e15c65b4304cc26732e2b7d3fb2c29674138af39
estewart97 <eoin.stewart@aquaq.co.uk>
2020-01-16 10:07:57 +0000

    Remove unnecessary components (#18)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React, { Component } from 'react'
 import LogoLeft from './logo-left.png'
 import LogoRight from './logo-right.png'
 import Data1 from './Data1'
-// import BarChart from './BarChart'
-// import PriceChart from './PriceChart'
-// import PriceChart1 from './PriceChart1'
 import MovingAverage from './MovingAverage'
 import Volatility from './Volatility'
 import PriceChartFinal from './PriceChartFinal'
-// import PriceChartFinal from './PriceChartFinal'
-//import SumBox from './SumBox'
 
 class App extends Component {
     state = {
