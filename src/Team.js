701c6806b26f02e3f994eddd2696d52da240f40e
robsketch <55433774+robsketch@users.noreply.github.com>
2020-01-17 16:02:22 +0000

    added a sweet ass photo of us

--- src/Team.js
+++ src/Team.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import Image from 'react-image-resizer'
 import './index.css'
-import Logo from './logo-left.png';
-import Logo2 from './logo-right.png';
+import Matt from './matt.jpg';
+import Rob from './rob.png'
+import Eoin from './eoin.jpg'
+import Phil from './phil.jpg'
+import Andy from './andrew.jpg'
+import TheTeam from './theteam.jpg'
 
 function Team() {
     return (
-        <div id ="team">
-            <Image src={Logo} alt="website logo" height="100" width="100" className="myImage"/>
-            <Image src={Logo2} alt="website logo" height="100" width="100" className="myImage"/>
+        <>
+        <h1>The Team</h1>
+        <div className="team">
+        <div class="member">
+                <h3>Phillip Gunning</h3>
+                <p>Didn't do stuff for his team</p>
+            </div>
+            <div class="member">
+                <h3>Matthew Greenlees</h3>
+                <p>Did stuff for his team</p>
+            </div>
+            <div class="member">
+                <h3>Robert Sketch</h3>
+                <p>The Americant</p>
+            </div>
+            <div class="member">
+                <h3>Eoin Stewart</h3>
+                <p>Also did stuff for his team</p>
+            </div>
+            <div class="member">
+                <h3>Andrew McNaught</h3>
+                <p>Literally did nothing</p>
+            </div>
         </div>
+        <img src={TheTeam} className="teamPic" />
+        </>
     );
 }
 
